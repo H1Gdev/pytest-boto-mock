0edbf6de02ef93a02d505e3f12ebfbd058149070
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2024-05-29 15:28:43 +0900

    Tests

--- tests/test_pytest_boto_mock.py
+++ tests/test_pytest_boto_mock.py
@@ -1,3 +1,4 @@
+import uuid
 import json
 
 import boto3
@@ -26,7 +27,7 @@ def test_lambda_value(boto_mocker, expected):
     }))
 
     actual = boto3.client('lambda').invoke(FunctionName='FunctionName')
-    assert expected == actual
+    assert actual == expected
 
 
 @pytest.mark.parametrize('expected', [
@@ -44,7 +45,7 @@ def test_lambda_callable(boto_mocker, expected):
     }))
 
     actual = boto3.client('lambda').invoke(FunctionName='FunctionName')
-    assert expected == actual
+    assert actual == expected
 
 
 @pytest.mark.parametrize('expected', [
@@ -58,7 +59,7 @@ def test_lambda_exception(boto_mocker, expected):
 
     with pytest.raises(Exception) as ex:
         boto3.client('lambda').invoke(FunctionName='FunctionName')
-        assert expected == ex
+        assert ex == expected
 
 
 @pytest.mark.parametrize('expected', [
@@ -79,10 +80,11 @@ def test_lambda_invoke_value(boto_mocker, expected):
     }))
 
     response = boto3.client('lambda').invoke(FunctionName='FunctionName')
+    assert response.get('StatusCode') == 200
     actual = response.get('Payload').read().decode()
     if actual:
         actual = json.loads(actual)
-    assert expected == actual
+    assert actual == expected
 
 
 @pytest.mark.parametrize('expected', [
@@ -102,7 +104,7 @@ def test_lambda_invoke_exception(boto_mocker, expected):
 
     with pytest.raises(Exception) as ex:
         boto3.client('lambda').invoke(FunctionName='FunctionName')
-        assert expected == ex
+        assert ex == expected
 
 
 @pytest.mark.parametrize('expected', [
@@ -122,9 +124,28 @@ def test_lambda_invoke_function_error(boto_mocker, expected):
     }))
 
     response = boto3.client('lambda').invoke(FunctionName='FunctionName')
+    assert response.get('StatusCode') == 200
     payload = json.loads(response.get('Payload').read())
     actual = payload.get('errorMessage')
-    assert str(expected) == actual
+    assert actual == str(expected)
+
+
+def test_lambda_invoke_event(boto_mocker):
+    boto_mocker.patch(new=boto_mocker.build_make_api_call({
+        'lambda': {
+            'Invoke': boto_mocker.build_lambda_invoke_handler({
+                'FunctionName': {
+                    'ResponseMetadata': {'HTTPStatusCode': 202},
+                    'StatusCode': 202,
+                    'Payload': '',
+                }
+            })
+        }
+    }))
+
+    response = boto3.client('lambda').invoke(FunctionName='FunctionName', InvocationType='Event')
+    assert response.get('StatusCode') == 202
+    response.get('Payload').read()
 
 
 # S3
@@ -148,7 +169,7 @@ def test_s3_value(boto_mocker, expected):
     }))
 
     actual = boto3.client('s3').copy_object(Bucket='bucket')
-    assert expected == actual
+    assert actual == expected
 
 
 @pytest.mark.parametrize('expected', [
@@ -166,7 +187,7 @@ def test_s3_callable(boto_mocker, expected):
     }))
 
     actual = boto3.client('s3').copy_object(Bucket='bucket')
-    assert expected == actual
+    assert actual == expected
 
 
 @pytest.mark.parametrize('expected', [
@@ -180,4 +201,103 @@ def test_s3_exception(boto_mocker, expected):
 
     with pytest.raises(Exception) as ex:
         boto3.client('s3').copy_object(Bucket='bucket')
-        assert expected == ex
+        assert ex == expected
+
+
+@pytest.mark.parametrize('count', [
+    0,
+    2,
+])
+def test_s3_resource(boto_mocker, count):
+    def list_objects(self, operation_name, kwarg):
+        ret = {
+            'ResponseMetadata': {'HTTPStatusCode': 200},
+            'IsTruncated': False,
+            'Name': 'bucket',
+            'Prefix': 'test',
+            'MaxKeys': 1000,
+        }
+        if count:
+            ret['Contents'] = [{'Key': f"test_{i}.txt"} for i in range(count)]
+        return ret
+
+    def delete_objects(self, operation_name, kwarg):
+        ret = {
+            'ResponseMetadata': {'HTTPStatusCode': 200},
+        }
+        if count:
+            ret['Deleted'] = [{'Key': f"test_{i}.txt"} for i in range(count)]
+        return ret
+
+    boto_mocker.patch(new=boto_mocker.build_make_api_call({
+        's3': {
+            'ListObjects': list_objects,
+            'DeleteObjects': delete_objects,
+        },
+    }))
+
+    boto3.resource('s3').Bucket('bucket').objects.filter(Prefix='test').delete()
+
+
+# SQS
+@pytest.fixture
+def setup_sqs(boto_mocker):
+    message_list = {}
+
+    def send_message(self, operation_name, kwarg):
+        queue_url = kwarg['QueueUrl']
+        message = {
+            'Body': kwarg['MessageBody'],
+            'ReceiptHandle': str(uuid.uuid4()),
+        }
+
+        nonlocal message_list
+        if queue_url in message_list:
+            message_list[queue_url].append(message)
+        else:
+            message_list[queue_url] = [message]
+
+        return {
+            'ResponseMetadata': {'HTTPStatusCode': 200},
+        }
+
+    def receive_message(self, operation_name, kwarg):
+        queue_url = kwarg['QueueUrl']
+        max_number_of_messages = kwarg.get('MaxNumberOfMessages', 1)
+
+        nonlocal message_list
+        ret = {
+            'ResponseMetadata': {'HTTPStatusCode': 200},
+        }
+        messages = message_list[queue_url][:max_number_of_messages]
+        if messages:
+            ret['Messages'] = messages
+        return ret
+
+    def delete_message(self, operation_name, kwarg):
+        queue_url = kwarg['QueueUrl']
+        receipt_handle = kwarg['ReceiptHandle']
+
+        nonlocal message_list
+        message_list[queue_url] = [message for message in message_list[queue_url] if message['ReceiptHandle'] != receipt_handle]
+        ret = {
+            'ResponseMetadata': {'HTTPStatusCode': 200},
+        }
+        return ret
+
+    boto_mocker.patch(new=boto_mocker.build_make_api_call({
+        'sqs': {
+            'SendMessage': send_message,
+            'ReceiveMessage': receive_message,
+            'DeleteMessage': delete_message,
+        },
+    }))
+
+
+def test_sqs_sequence(setup_sqs):
+    queue_url = 'https://sqs.REGION.amazonaws.com/ACCOUNT_ID/QueueName.fifo'
+    sqs = boto3.client('sqs')
+    response = sqs.send_message(QueueUrl=queue_url, MessageBody='Body', MessageGroupId='GroupId', MessageDeduplicationId='DeduplicationId')
+    response = sqs.receive_message(QueueUrl=queue_url)
+    receipt_handle = response['Messages'][0]['ReceiptHandle']
+    response = sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
