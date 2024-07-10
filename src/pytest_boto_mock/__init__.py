87a77be95ad55b5d63bc42a17a48090d8fd33ce8
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2024-07-10 21:38:07 +0900

    Versioning
    - __version__

--- src/pytest_boto_mock/__init__.py
+++ src/pytest_boto_mock/__init__.py
@@ -1,3 +1,4 @@
+from pytest_boto_mock._version import __version__
 from pytest_boto_mock.plugin import boto_mocker
 from pytest_boto_mock.plugin import class_boto_mocker
 from pytest_boto_mock.plugin import module_boto_mocker
@@ -6,6 +7,7 @@ from pytest_boto_mock.plugin import session_boto_mocker
 
 
 __all__ = [
+    '__version__',
     'boto_mocker',
     'class_boto_mocker',
     'module_boto_mocker',
