73c292e58dcad301bf9b655705a54bb4e5550d01
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2026-09-09 17:59:19 +0900

    Lint
    - Ruff

--- tests/test_package.py
+++ tests/test_package.py
@@ -4,12 +4,15 @@ import pytest_boto_mock
 
 
 def test_version():
-    pytest_boto_mock.__version__
+    assert hasattr(pytest_boto_mock, '__version__')
+    assert type(pytest_boto_mock.__version__) is str
 
 
 def test_boto3_version():
-    boto3.__version__
+    assert hasattr(boto3, '__version__')
+    assert type(boto3.__version__) is str
 
 
 def test_botocore_version():
-    botocore.__version__
+    assert hasattr(botocore, '__version__')
+    assert type(botocore.__version__) is str
